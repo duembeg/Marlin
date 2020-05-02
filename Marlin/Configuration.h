406810b533f98cd0f1ebeb09e1d56dd8af879d86
Wilhelm Duembeg <wilhelm.duembeg@dura-machines.com>
2020-05-02 14:42:24 -0700

    Made branch for specific beta-test printer

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(wduembeg Rep2 Evo config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(wduembeg Rep2 Evo noiman-uc config)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
